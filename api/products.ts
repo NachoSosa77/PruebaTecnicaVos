7db2dc09bf9e588e9f164ba923564f8a04bbaaf2
Ignacio Sosa <isosa@forest.local>
2024-08-30 18:25:15 -0300

    Second Commit

--- api/products.ts
+++ api/products.ts
@@ -13,7 +13,7 @@ export async function getProducts(): Promise<Product[]> {
     const res = await fetch(urlApi);
     const json = await res.json();
     return Promise.resolve(
-        json.products.map((product) => ({ id: product.id, title: product.title, image: product.image, price: product.price }))
+        json.products.map((product) => ({ id: product.id, title: product.title, image: product.image, price: product.price, category: product.category }))
     )
 
 
@@ -22,6 +22,6 @@ export async function getProducts(): Promise<Product[]> {
 export async function getProductDetail(id: string): Promise<Product> {
     const res = await fetch(urlApi);
     const result = await res.json();
-    return Promise.resolve(result[0] as Product);
-
+    const product = result.filter((item)=>item.id === id)
+    return Promise.resolve(product[0] as Product)
 } 
